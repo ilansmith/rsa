28e68f9ddb20e2215ee74725fac0b9c0c1e368bf
Ilan Smith <lunnys@gmail.com>
2018-07-15 03:44:40 +0300

    license - parse arguments
    Signed-off-by: Ilan Smith <lunnys@gmail.com>

--- rsa_license.h
+++ rsa_license.h
@@ -4,65 +4,11 @@
 #include "rsa_num.h"
 #include "rsa_util.h"
 
-typedef int (*lic_create_t)(char *buf, int len, void *data);
-typedef int (*lic_info_t)(char *buf, int len);
-typedef int (*lic_extract_t)(char *buf, int len, void *data);
-
 struct rsa_license_ops {
-	lic_create_t lic_create;
-	lic_info_t lic_info;
-	lic_extract_t lic_extract;
+	int (*lic_create)(char *buf, int len, void *data);
+	int (*lic_info)(char *buf, int len);
+	int (*lic_extract)(char *buf, int len, void *data);
 };
-
-#define ARG "arg"
-
-#define	RSA_TBD(msg) printf("TBD: %s\n", (msg))
-#define OPT_FLAG(OPT) (1 << (OPT))
-
-#define OPT_ADD(flags, OPT) do { \
-	if (*(flags) & OPT_FLAG(OPT)) { \
-		rsa_error_message(RSA_ERR_ARGREP); \
-		return -1; \
-	} \
-	*(flags) |= OPT_FLAG(OPT); \
-} while (0)
-
-typedef enum {
-	/* actions */
-	RSA_OPT_HELP,
-	RSA_OPT_KEY_SCAN,
-	RSA_OPT_KEY_SET_DEFAULT,
-	RSA_OPT_KEY_SET_DYNAMIC,
-	RSA_OPT_PATH,
-	RSA_OPT_QUITE,
-	RSA_OPT_VERBOSE,
-	RSA_OPT_ENCRYPT,
-	RSA_OPT_DECRYPT,
-	RSA_OPT_KEYGEN,
-	/* non actions */
-	RSA_OPT_LEVEL,
-	RSA_OPT_RSAENC,
-	RSA_OPT_CBC,
-	RSA_OPT_FILE,
-	RSA_OPT_ENC_INFO_ONLY,
-	RSA_OPT_ORIG_FILE,
-	RSA_OPT_MAX
-} rsa_opt_t;
-
-typedef struct opt_t {
-	int code;
-	char short_opt;
-	char *long_opt;
-	int arg_required;
-	char *description;
-} opt_t;
-
-typedef struct {
-	char keytype;
-	opt_t *options;
-	int (*ops_handler)(int code, unsigned int *flags);
-	int (*ops_handler_finalize)(unsigned int *flags, int actions);
-} rsa_handler_t ;
 
 int rsa_license_create(char *priv_key_path, char *file_name, 
 		struct rsa_license_ops *license_ops, void *data);
