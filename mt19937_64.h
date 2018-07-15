71e7846040f69dd7d1f31cc68abcb9d8be89ea31
Ilan Smith <lunnys@gmail.com>
2018-07-15 03:42:03 +0300

    Added Nishimura & Matsumoto's 64bit Mersenne Twister PRNG.
    Takuji Nishimura and Makoto Matsumoto's published a 64-bit version of
    the Mersenne Twister pseudorandom number generator.
    
    Change-Id: I9ff4cd3c35545601d2d286149d37620ec9e07cb2
    Signed-off-by: Ilan Smith <lunnys@gmail.com>

---
+++ mt19937_64.h
@@ -0,0 +1,27 @@
+#ifndef _MT19937_64_
+#define _MT19937_64_
+
+/* initializes mt[NN] with a seed */
+void init_genrand64(unsigned long long seed);
+
+/* initialize by an array with array-length */
+/* init_key is the array for initializing keys */
+/* key_length is its length */
+void init_by_array64(unsigned long long init_key[], 
+    unsigned long long key_length);
+
+/* generates a random number on [0, 2^64-1]-interval */
+unsigned long long genrand64_int64(void);
+
+/* generates a random number on [0, 2^63-1]-interval */
+long long genrand64_int63(void);
+
+/* generates a random number on [0,1]-real-interval */
+double genrand64_real1(void);
+
+/* generates a random number on [0,1)-real-interval */
+double genrand64_real2(void);
+
+/* generates a random number on (0,1)-real-interval */
+double genrand64_real3(void);
+#endif
