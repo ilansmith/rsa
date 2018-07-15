85c5ab9aa1acf394396de6ba2b41ad0e91e6a5fd
Ilan Smith <lunnys@gmail.com>
2018-07-15 03:42:04 +0300

    Enhanced RSA key maintanence.
    - Added RSA key setting
    - Warn when multiple entries for the same keyname
    - Gard from creating key files with the names of the key links
    - Changed the error message function to receive a variable amount of
      arguments
    
    Change-Id: I710dd40bf1e89a4e92a74e4f52451179c8e2e834
    Signed-off-by: Ilan Smith <lunnys@gmail.com>

--- rsa_util.h
+++ rsa_util.h
@@ -10,6 +10,10 @@
 #define ARRAY_SZ(arr) (sizeof(arr) / sizeof(arr[0]))
 #define IS_WHITESPACE(c) ((c) == ' ' || (c) == '\t')
 
+#define C_GREY "\033[00;37m"
+#define C_NORMAL "\033[00;00;00m"
+#define C_HIGHLIGHT "\033[01m"
+
 typedef struct code2code_t {
     int code;
     int val;
@@ -40,6 +44,8 @@ typedef enum {
     RSA_ERR_KEYPATH,
     RSA_ERR_KEYNAME,
     RSA_ERR_KEYGEN,
+    RSA_ERR_KEYNOTEXIST,
+    RSA_ERR_KEYMULTIENTRIES,
     RSA_ERR_LEVEL,
     RSA_ERR_INTERNAL
 } rsa_errno_t;
@@ -47,9 +53,11 @@ typedef enum {
 int code2code(code2code_t *list, int code);
 char *code2str(code2str_t *list, int code);
 
-void output_error_message(rsa_errno_t err);
+void rsa_error_message(rsa_errno_t err, ...);
+void rsa_warning_message(rsa_errno_t err, ...);
 int rsa_printf(int is_verbose, int ind, char *fmt, ...);
 char *rsa_strcat(char *dest, char *fmt, ...);
+char *rsa_vstrcat(char *dest, char *fmt, va_list ap);
 int rsa_sprintf_nows(char *str, char *fmt, ...);
 int rsa_read_u1024(FILE *file, u1024_t *num);
 int rsa_write_u1024(FILE *file, u1024_t *num);
